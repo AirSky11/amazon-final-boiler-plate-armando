1d296d8257cbe581c9ca9c081b979c2ec5ca759d
AirSky11 <armando.ayerbe@gmail.com>
2023-07-29 02:56:15 +0200

    actualizacion sin error

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -4,7 +4,7 @@
   <div id="container" class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
 
     <div class="logo">
-      <img src="../img/logo-mascaras.png" alt="">
+      <img src="" alt="">
     </div> 
 
     <div class="header">
